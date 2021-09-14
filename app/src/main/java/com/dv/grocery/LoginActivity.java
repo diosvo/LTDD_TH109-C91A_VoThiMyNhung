bd13069378ef44b6ff9548e5af89ad4ec47dead5
Dios Vo <vtmn1212@gmail.com>
2021-09-14 21:34:43 +0700

    Create functions between Register and Login page

--- app/src/main/java/com/dv/grocery/LoginActivity.java
+++ app/src/main/java/com/dv/grocery/LoginActivity.java
@@ -1,14 +1,44 @@
 package com.dv.grocery;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class LoginActivity extends AppCompatActivity {
+
+    Button login;
+    EditText email, password;
+    TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initValues();
+        onLogin();
+        onRegister();
+    }
+
+    private void initValues() {
+        login = findViewById(R.id.btn_login);
+        register = findViewById(R.id.txt_register);
+
+        email = findViewById(R.id.email_register);
+        password = findViewById(R.id.password_register);
+    }
+
+
+    void onRegister() {
+        register.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, RegisterActivity.class)));
+    }
+
+    void onLogin() {
+        login.setOnClickListener(view -> {
+        });
     }
 }
--- app/src/main/java/com/dv/grocery/RegisterActivity.java
+++ app/src/main/java/com/dv/grocery/RegisterActivity.java
@@ -1,24 +1,44 @@
 package com.dv.grocery;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Paint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class RegisterActivity extends AppCompatActivity {
+    Button register;
+    EditText fullName, email, password;
+    TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        setTextUnderline();
+        initValues();
+        onRegister();
+        onLogin();
     }
 
-    private void setTextUnderline() {
-        TextView textView = (TextView) findViewById(R.id.btn_login);
-        textView.setPaintFlags(textView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+    void initValues() {
+        register = findViewById(R.id.btn_register);
+        login = findViewById(R.id.txt_login);
+
+        fullName = findViewById(R.id.full_name);
+        email = findViewById(R.id.email_register);
+        password = findViewById(R.id.password_register);
+    }
+
+    void onLogin() {
+        login.setOnClickListener(view -> startActivity(new Intent(RegisterActivity.this, LoginActivity.class)));
+    }
+
+    void onRegister() {
+        register.setOnClickListener(view -> {
+
+        });
     }
 }
