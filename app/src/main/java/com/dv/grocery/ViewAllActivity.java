a7d5299b3e50ca65a9cb1ceb4ab98629995cfeb9
Dios Vo <vtmn1212@gmail.com>
2021-09-20 22:43:25 +0700

    - Update Category: click on item to view all products by this group - Change class name: ViewAllActivity - Update category UI

--- app/src/main/java/com/dv/grocery/ViewAllActivity.java
+++ app/src/main/java/com/dv/grocery/ViewAllActivity.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class ViewAlProductsByGroupActivity extends AppCompatActivity {
+public class ViewAllActivity extends AppCompatActivity {
     TextView title;
 
     FirebaseFirestore db;
