adbc854dacd5e81f6b9aab4e18d0d05b41e7944f
Dios Vo <vtmn1212@gmail.com>
2021-09-21 21:52:56 +0700

    fecth product details into UI

--- app/src/main/java/com/dv/grocery/adapters/ViewAllAdapter.java
+++ app/src/main/java/com/dv/grocery/adapters/ViewAllAdapter.java
@@ -1,6 +1,7 @@
 package com.dv.grocery.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.dv.grocery.R;
+import com.dv.grocery.activities.DetailsActivity;
 import com.dv.grocery.models.ProductModel;
 
 import java.util.List;
@@ -29,7 +31,7 @@ public class ViewAllAdapter extends RecyclerView.Adapter<ViewAllAdapter.ViewHold
     @NonNull
     @Override
     public ViewAllAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ViewAllAdapter.ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.view_all_item, parent, false));
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.view_all_item, parent, false));
     }
 
     @Override
@@ -38,6 +40,12 @@ public class ViewAllAdapter extends RecyclerView.Adapter<ViewAllAdapter.ViewHold
         holder.va_name.setText(productModelList.get(position).getName());
         holder.va_price.setText(productModelList.get(position).getPrice());
         holder.va_desc.setText(productModelList.get(position).getDescription());
+
+        holder.itemView.setOnClickListener(view -> {
+            Intent intent = new Intent(context, DetailsActivity.class);
+            intent.putExtra("details", productModelList.get(position));
+            context.startActivity(intent);
+        });
     }
 
     @Override
@@ -45,7 +53,7 @@ public class ViewAllAdapter extends RecyclerView.Adapter<ViewAllAdapter.ViewHold
         return productModelList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         ImageView va_img;
         TextView va_name, va_price, va_desc;
 
