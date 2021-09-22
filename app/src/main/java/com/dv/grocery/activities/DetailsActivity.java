0561d5d55620fe80525d2b9e51bb50d6306589c5
Dios Vo <vtmn1212@gmail.com>
2021-09-22 21:37:29 +0700

    feature: Add to cart (save order details, current user)

--- app/src/main/java/com/dv/grocery/activities/DetailsActivity.java
+++ app/src/main/java/com/dv/grocery/activities/DetailsActivity.java
@@ -1,16 +1,26 @@
 package com.dv.grocery.activities;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.dv.grocery.R;
 import com.dv.grocery.models.ProductModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -22,11 +32,15 @@ public class DetailsActivity extends AppCompatActivity {
     Button plus, minus, add_to_cart;
 
     ProductModel productModel = null;
+    FirebaseFirestore db;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
 
         final Object object = getIntent().getSerializableExtra("details");
         if (object instanceof ProductModel) {
@@ -35,6 +49,7 @@ public class DetailsActivity extends AppCompatActivity {
 
         init();
         updateQty();
+        addToCart();
 
         if (productModel != null) {
             Glide.with(getApplicationContext()).load(productModel.getImage()).into(detail_img);
@@ -63,6 +78,7 @@ public class DetailsActivity extends AppCompatActivity {
             if (totalQty < 10) {
                 totalQty++;
                 quantity.setText(String.valueOf(totalQty));
+                totalPrice = Integer.parseInt(productModel.getPrice()) * totalQty;
             }
         });
 
@@ -70,7 +86,39 @@ public class DetailsActivity extends AppCompatActivity {
             if (totalQty > 1) {
                 totalQty--;
                 quantity.setText(String.valueOf(totalQty));
+                totalPrice = Integer.parseInt(productModel.getPrice()) * totalQty;
             }
         });
+    }
+
+    private void addToCart() {
+        add_to_cart.setOnClickListener(view -> {
+            String saveCurrentDate, saveCurrentTime;
+            Calendar calendar = Calendar.getInstance();
+
+            SimpleDateFormat currentDate = new SimpleDateFormat("dd/MM/yyyy");
+            saveCurrentDate = currentDate.format(calendar.getTime());
+
+
+            SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss");
+            saveCurrentTime = currentTime.format(calendar.getTime());
+
+            final HashMap<String, Object> cartMap = new HashMap<>();
+            cartMap.put("productName", productModel.getName());
+            cartMap.put("productPrice", productModel.getPrice());
+            cartMap.put("currentDate", saveCurrentDate);
+            cartMap.put("currentTime", saveCurrentTime);
+            cartMap.put("totalQuantity", quantity.getText().toString());
+            cartMap.put("totalPrice", totalPrice);
+
+            db.collection("AddToCart")
+                .document(auth.getCurrentUser().getUid())
+                .collection("CurrentUser")
+                .add(cartMap)
+                .addOnCompleteListener(task -> {
+                    Toast.makeText(DetailsActivity.this, "Đã thêm vào giỏ hàng", Toast.LENGTH_SHORT).show();
+                });
+        });
+
     }
 }
