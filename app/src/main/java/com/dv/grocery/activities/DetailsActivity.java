618926409df65c61a183283eda1c8362b7e56300
Dios Vo <vtmn1212@gmail.com>
2021-09-22 20:42:47 +0700

    update qty

--- app/src/main/java/com/dv/grocery/activities/DetailsActivity.java
+++ app/src/main/java/com/dv/grocery/activities/DetailsActivity.java
@@ -1,6 +1,7 @@
 package com.dv.grocery.activities;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,6 +16,9 @@ public class DetailsActivity extends AppCompatActivity {
 
     ImageView detail_img;
     TextView name, desc, price, quantity;
+    int totalQty = 1;
+    int totalPrice = 0;
+
     Button plus, minus, add_to_cart;
 
     ProductModel productModel = null;
@@ -30,12 +34,15 @@ public class DetailsActivity extends AppCompatActivity {
         }
 
         init();
+        updateQty();
 
         if (productModel != null) {
             Glide.with(getApplicationContext()).load(productModel.getImage()).into(detail_img);
             name.setText(productModel.getName());
             desc.setText(productModel.getDescription());
             price.setText(productModel.getPrice());
+
+            totalPrice = Integer.parseInt(productModel.getPrice()) * totalQty;
         }
     }
 
@@ -49,5 +56,21 @@ public class DetailsActivity extends AppCompatActivity {
         plus = findViewById(R.id.detail_plus);
         minus = findViewById(R.id.detail_minus);
         add_to_cart = findViewById(R.id.detail_add_to_cart);
+    }
+
+    private void updateQty() {
+        plus.setOnClickListener(view -> {
+            if (totalQty < 10) {
+                totalQty++;
+                quantity.setText(String.valueOf(totalQty));
+            }
+        });
+
+        minus.setOnClickListener(view -> {
+            if (totalQty > 1) {
+                totalQty--;
+                quantity.setText(String.valueOf(totalQty));
+            }
+        });
     }
 }
