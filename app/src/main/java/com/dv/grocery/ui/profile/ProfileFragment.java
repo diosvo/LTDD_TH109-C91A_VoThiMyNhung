07ac036a1ad39ef4ba5f36a373b9003773a0a2a1
Dios Vo <vtmn1212@gmail.com>
2021-09-25 21:09:42 +0700

    Update user info (image included) from Database

--- app/src/main/java/com/dv/grocery/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/dv/grocery/ui/profile/ProfileFragment.java
@@ -14,13 +14,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.dv.grocery.R;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.dv.grocery.models.UserModel;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -29,20 +32,21 @@ public class ProfileFragment extends Fragment {
     CircleImageView image;
     Button updateBtn;
 
-    FirebaseFirestore db;
+    FirebaseDatabase db;
     FirebaseAuth auth;
     FirebaseStorage storage;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        db = FirebaseFirestore.getInstance();
+        db = FirebaseDatabase.getInstance();
         auth = FirebaseAuth.getInstance();
         storage = FirebaseStorage.getInstance();
 
         init(root);
         uploadImage();
         onUpdateProfile();
+        updateFromDatabase();
 
         return root;
     }
@@ -67,12 +71,27 @@ public class ProfileFragment extends Fragment {
     }
 
     private void onUpdateProfile() {
-        updateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        updateBtn.setOnClickListener(view -> {
 
-            }
         });
+    }
+
+    private void updateFromDatabase() {
+        db.getReference()
+            .child("Users")
+            .child(auth.getUid())
+            .addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    UserModel userModel = snapshot.getValue(UserModel.class);
+                    Glide.with(getContext()).load(userModel.getProfileImage()).into(image);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
     }
 
     @Override
@@ -85,13 +104,18 @@ public class ProfileFragment extends Fragment {
 
             final StorageReference storageRef = storage.getReference()
                 .child("profile_image")
-                .child(FirebaseAuth.getInstance().getUid());
-            storageRef.putFile(profileUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                .child(auth.getUid());
+
+            storageRef.putFile(profileUri).addOnSuccessListener(taskSnapshot ->
+                storageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                    db.getReference()
+                        .child("Users")
+                        .child(auth.getUid())
+                        .child("profileImage")
+                        .setValue(uri.toString());
+
                     Toast.makeText(getContext(), "Hình ảnh đã được cập nhật.", Toast.LENGTH_SHORT).show();
-                }
-            });
+                }));
         }
     }
 }
