b7b05d2cab90ba4b19bff1b7ebd9566df233c76b
Dios Vo <vtmn1212@gmail.com>
2021-09-26 16:24:45 +0700

    Remove rcm products section

--- app/src/main/java/com/dv/grocery/ui/home/HomeFragment.java
+++ app/src/main/java/com/dv/grocery/ui/home/HomeFragment.java
@@ -19,10 +19,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.dv.grocery.R;
 import com.dv.grocery.adapters.HomeCategoryAdapter;
 import com.dv.grocery.adapters.PopularAdapter;
-import com.dv.grocery.adapters.RecommendedAdapter;
 import com.dv.grocery.adapters.ViewAllAdapter;
 import com.dv.grocery.models.CategoryModel;
 import com.dv.grocery.models.ProductModel;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -35,8 +36,10 @@ public class HomeFragment extends Fragment {
     ScrollView scrollView;
     ProgressBar progressBar;
 
-    RecyclerView popularRec, categoryRec, recommendedRec;
+    RecyclerView popularRec;
+    RecyclerView categoryRec;
     FirebaseFirestore db;
+    DatabaseReference rootRef;
 
     // Popular Products
     List<ProductModel> popularProductsList;
@@ -46,14 +49,9 @@ public class HomeFragment extends Fragment {
     List<CategoryModel> categoryList;
     HomeCategoryAdapter categoryAdapter;
 
-    // Recommend Products
-    List<ProductModel> rcmProductsList;
-    RecommendedAdapter recommendedAdapter;
-
     // Search View
     EditText searchText;
     private List<ProductModel> searchList;
-    private RecyclerView searchRec;
     private ViewAllAdapter viewAllAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -62,17 +60,16 @@ public class HomeFragment extends Fragment {
 
         popularRec = root.findViewById(R.id.popular_products);
         categoryRec = root.findViewById(R.id.home_category);
-        recommendedRec = root.findViewById(R.id.recommend_products);
 
         scrollView = root.findViewById(R.id.home_scroll_view);
         progressBar = root.findViewById(R.id.home_progress_bar);
         showProgressBar();
 
         db = FirebaseFirestore.getInstance();
+        rootRef = FirebaseDatabase.getInstance().getReference();
 
         getPopularProducts();
         getHomeCategory();
-        getRecommendProducts();
         searchProducts(root);
 
         return root;
@@ -122,31 +119,9 @@ public class HomeFragment extends Fragment {
             });
     }
 
-    private void getRecommendProducts() {
-        recommendedRec.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
-        rcmProductsList = new ArrayList<>();
-        recommendedAdapter = new RecommendedAdapter(getActivity(), rcmProductsList);
-        recommendedRec.setAdapter(recommendedAdapter);
-
-        db.collection(getString(R.string.recommended_products))
-            .get()
-            .addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
-                        ProductModel rcmProductModel = document.toObject(ProductModel.class);
-                        rcmProductsList.add(rcmProductModel);
-                        recommendedAdapter.notifyDataSetChanged();
-                        hideProgressBar();
-                    }
-                } else {
-                    Toast.makeText(getActivity(), "Error: " + task.getException(), Toast.LENGTH_SHORT).show();
-                }
-            });
-    }
-
     private void searchProducts(View root) {
         searchText = root.findViewById(R.id.search_box);
-        searchRec = root.findViewById(R.id.search_rec);
+        RecyclerView searchRec = root.findViewById(R.id.search_rec);
         searchList = new ArrayList<>();
         viewAllAdapter = new ViewAllAdapter(getContext(), searchList);
 
@@ -172,23 +147,21 @@ public class HomeFragment extends Fragment {
                     searchList.clear();
                     viewAllAdapter.notifyDataSetChanged();
                 } else {
-                    if (!name.isEmpty()) {
-                        db.collection(getString(R.string.popular_products))
-                            .whereEqualTo("group", name)
-                            .get()
-                            .addOnCompleteListener(task -> {
-                                if (task.isSuccessful() && task.getResult() != null) {
-                                    searchList.clear();
-                                    viewAllAdapter.notifyDataSetChanged();
+                    db.collection(getString(R.string.popular_products))
+                        .whereEqualTo("group", name)
+                        .get()
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful() && task.getResult() != null) {
+                                searchList.clear();
+                                viewAllAdapter.notifyDataSetChanged();
 
-                                    for (DocumentSnapshot doc : task.getResult().getDocuments()) {
-                                        ProductModel productModel = doc.toObject(ProductModel.class);
-                                        searchList.add(productModel);
-                                        viewAllAdapter.notifyDataSetChanged();
-                                    }
+                                for (DocumentSnapshot doc : task.getResult().getDocuments()) {
+                                    ProductModel productModel = doc.toObject(ProductModel.class);
+                                    searchList.add(productModel);
+                                    viewAllAdapter.notifyDataSetChanged();
                                 }
-                            });
-                    }
+                            }
+                        });
                 }
             }
         });
